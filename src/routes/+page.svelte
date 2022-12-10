d4f40c5c9378646321e6b32ef0cd0a3f41bffa0f
Adam Rackis <adamrackis@hotmail.com>
2022-12-09 20:31:53 -0600

    Bug repro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import Component from "./Component.svelte";
+
+  let value = 0;
+  let update = () => (value = value + 1);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Component {value} {update} />
---
+++ src/routes/Component.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let value: any;
+  export let update: any;
+</script>
+
+<form>
+  <input autofocus />
+</form>
+
+{value}
+
+<br />
+<a on:click={update} on:keypress={() => {}}>Bad link - no href (will refocus)</a>
+
+<br />
+<br />
+<a href="https://xhamster.com" on:click={(e) => e.preventDefault()} on:keypress={() => {}}>
+  Good link - has href with prevent default (will not refocus)</a
+>
+
+<br />
+<br />
+<button on:click={update} on:keypress={() => {}}>Update (will not refocus - which is correct)</button>
+
+<style>
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
